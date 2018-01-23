528bc9ef67486202e7e61cbf6c18a84cf69f63bc
joeflack4 <joeflack4@gmail.com>
2018-01-22 22:49:26 -0500

    Updated requirements.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
-# Dependencies: OpenCV 3, Scikit, Scipy, Numpy+mkl, Tesseract, pytesseract, jellyfish.
-# PIL  <- no? wasn't mentioned in readme documentation
-conda install numpy &
-# cv2  # install 'opencv' instead
-conda install opencv &
-conda install -c conda-forge jellyfish &
-# Scikit <-- scikit-learn
-conda install scikit-learn &
-# Scipy
-conda install -c anaconda scipy &
-# skimage  <- even though not listed in the readme documentation, it threw an error without this.
-# hopefully the following installed the correct version:
+# Conda installs OpenCV 2.4.11.
+# For pip installs, see requirements.txt for versions.
+pip install imutils &
+pip install numpy &
 pip install scikit-image &
+conda install -c menpo opencv
+
+# - Other project
+# Dependencies: OpenCV 3, Scikit, Scipy, Numpy+mkl, Tesseract, pytesseract, jellyfish.
 # Tesseract
 #conda install -c bioconda tesseract &  # DIDNT WORK
 #conda install -c simonflueckiger tesserocr ?
 # hopefully the following installed the correct version:
-pip install tesseract &
+#    pip install tesseract &
 # pytesseract
 #conda install -c auto pytesseract &  # DIDNT WORK
 # hopefully the following installed the correct version:
-pip install pytesseract &
+#    pip install pytesseract &
+# PIL  <- no? wasn't mentioned in readme documentation
+# cv2  # install 'opencv' instead
+#     conda install opencv &
+#    conda install -c conda-forge jellyfish & #
+# Scikit <-- scikit-learn
+#    conda install scikit-learn & #
+# Scipy
+#    conda install -c anaconda scipy & #
+# skimage  <- even though not listed in the readme documentation, it threw an error without this.
+# hopefully the following installed the correct version:
 # Numpy+mkl
 #conda install -c intel/label/deprecated mkl
-conda install -c intel mkl
+#    conda install -c intel mkl #
